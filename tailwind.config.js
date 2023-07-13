0e3ff99864388173b8925be59a0d20aa2fd01a57
AlexDeft <107889269+AlexDeft@users.noreply.github.com>
2023-07-13 00:31:20 -0300

    ajustes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        fundo1: "url('/hero-image-1.png')",
+      },
+      spacing: {
+        450: '450px',
+      },
+    },
   },
   plugins: [],
 }
